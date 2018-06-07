8fd5b9593bc57b0172999731e044d140f1f62d83
vikaskyadav <vikasharry03@gmail.com>
2018-06-07 15:00:14 +0530

    Added content on Pages. Todo: add images or remove images

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -15,7 +15,7 @@ const Header = (props) => (
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Latest news and updates</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Current Goals</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -10,33 +10,77 @@ class Main extends React.Component {
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
 
-    return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+    return <div id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
+        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <p>
+            On March 1st, 2018, 9000 CryptoKitties players were chosen at
+            random to receive an airdrop of 100m AVO tokens each. The
+            reasoning for this was because CK players were recognized as
+            creative, innovative, and positive users of crypto. This action
+            has been dubbed the world's first reverse-ICO, bringing people
+            together for the purpose of community and collaboration.
+          </p>
+          <p>
+            Around the time of March 18th the original creator of AVO token
+            suddenly disappeared and took all projects and social media
+            platforms related to AVO with them. They also burned their own
+            supply of the tokens, reducing the number from 100 trillion to
+            900 billion that are now in circulation.
+          </p>
+          <p>
+            The community assembled here thus far had to ask: what do we do
+            with our AVO? It would seem that the creator of this token,
+            whoever they are, has left it up to us to decide.
+          </p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
+          <h2 className="major">Current Goals</h2>
+          <span className="image main">
+            <img src={pic02} alt="" />
+          </span>
+          <ul>
+            <li>
+              <p>
+                As of April 2018, we are still trying to reach many original
+                hodlers of AVO, while simultaneously trying to bring new and
+                interested users on board. A longer-term goal of 500-1000
+                users is desired.
+              </p>
+            </li>
+            <li>
+              <p>
+                We need a viable use case for AVO. The original project
+                included a clicker-type dapp and a possible partnership with
+                an ETH-based game/dapp. Other ideas include possibly
+                partnering with local grocery chains/farmer's markets.
+                Charity is also a viable option.
+              </p>
+            </li>
+          </ul>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
+          <h2 className="major">Bounties</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            AVO bounties will be placed on various tasks here. Please donate
+            AVO tokens for bounties to the Avocado Bounty Pool address here:
+            0x7d0ebffc56429c818f34d0b46e239436fbe0fa5f Small amounts of ETH
+            are also very welcome for transaction fees.
+          </p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Contact</h2>
           <form method="post" action="#">
             <div className="field half first">
@@ -49,24 +93,47 @@ class Main extends React.Component {
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" />
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li>
+              <a href="https://twitter.com/AvocadoToken" className="icon fa-twitter">
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/AVOToken/" className="icon fa-facebook">
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/avo_avocado_token/" className="icon fa-instagram">
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/AvocadoNetwork" className="icon fa-github">
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.reddit.com/user/hodlandfodl/comments/896zqm/cryptokitties_and_the_avo_token_connection/?st=jfs09en1&sh=b1a6c472" className="icon fa-reddit">
+                <span className="label">Reddit</span>
+              </a>
+            </li>
           </ul>
           {close}
         </article>
-
       </div>
-    )
   }
 }
 
